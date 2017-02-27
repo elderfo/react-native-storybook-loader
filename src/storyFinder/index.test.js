baf9169c4852ed3af7a0e13ac1511a788d6c7fae
Freddy <cgetsfred@gmail.com>
2017-02-26 21:34:38 -0600

    test(storyFinder): Added a test to confirm glob call

--- src/storyFinder/index.test.js
+++ src/storyFinder/index.test.js
@@ -10,17 +10,26 @@ import { loadStories } from './';
 jest.mock('glob');
 
 beforeEach(() => {
-  jest.resetAllMocks();
+  jest.clearAllMocks();
 });
 
 const files = [faker.system.fileName(), faker.system.fileName()];
 
-function setupMocks(sync) {
-  glob.sync = jest.fn(sync || (() => files));
+function setupMocks() {
+  glob.sync = jest.fn(() => files);
 }
 
 test('loadStories should locate stories', () => {
   setupMocks();
   const actual = loadStories();
   expect(actual).toBe(files);
+});
+
+test('loadStories should perform expected work', () => {
+  setupMocks();
+  const pattern = faker.system.fileName();
+
+  loadStories(pattern);
+
+  expect(glob.sync).toHaveBeenCalledWith(pattern);
 });
