f2367cfe4f5f25e15275bcaea44ab2c1846ed3d4
Chris Getsfred <cgetsfred@gmail.com>
2017-03-23 20:31:55 -0500

    feat(cli): Add cli options
    Added `--searchDir`, `--outputFile` and `--pattern` parameters to the cli
    
    Closes #15

--- src/rnstl-cli.js
+++ src/rnstl-cli.js
@@ -1,11 +1,33 @@
 #!/usr/bin/env node
+import yargs from 'yargs';
+
+import cliResolver from './paths/cliResolver';
 import { info } from './logger';
 import { writeOutStoryLoader } from './storyWriterProcess';
 import resolvePaths from './paths/multiResolver';
 
+const args = yargs
+  .usage('$0 [options]')
+  .options({
+    searchDir: {
+      array: true,
+      desc: 'The directory or directories, relative to the project root, to search for files in.',
+    },
+    pattern: {
+      desc: 'The directory or directories, relative to the project root, to search for files in.',
+      type: 'string',
+    },
+    outputFile: {
+      desc: 'The directory or directories, relative to the project root, to search for files in.',
+      type: 'string',
+    },
+  })
+  .help()
+  .argv;
+
+const cliConfig = cliResolver(args);
+
 const pathConfig = resolvePaths(process.cwd());
 info('\nGenerating Dynamic Storybook File List\n');
-info('package.json:     ', pathConfig.packageJsonFile);
-info('Base directory:   ', pathConfig.baseDir);
 
-writeOutStoryLoader(pathConfig);
+writeOutStoryLoader(pathConfig, cliConfig);
