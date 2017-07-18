dd849fbc9cc87e930630a61526a4b5b598e9a1d5
Chris Getsfred <cgetsfred@gmail.com>
2017-07-18 17:03:45 -0500

    fix: correct path separators during glob retrival
    When resolving globs in windows, the path separators could be wrong

---
+++ src/utils/pathHelper.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * Determines if the path is prefixed or not
+ *
+ * @param {String} relativePath - Relative path to check for directory prefixes
+ * @returns True if path prefix exists, otherwise false
+ */
+const hasPathPrefix = relativePath =>
+  relativePath.substr(0, 2) === '..'
+  || relativePath.substr(0, 2) === './'
+  || relativePath.substr(0, 2) === '.\\';
+
+/**
+ * Correctly formats path separators
+ *
+ * @param {String} path - Path to format
+ * @returns Path with the correct separators
+ */
+const formatPath = (dir, separator = path.sep) => {
+  const oppositeSep = separator === '/' ? '\\' : '/';
+  return dir.replace(new RegExp(`\\${oppositeSep}`, 'g'), separator);
+};
+
+/**
+ * Converts a path into a relative path
+ *
+ * @param {String} file - File to convert to a relative path
+ * @param {String} fromDir - Directory to resolve to
+ * @param {String} separator - Path separator character (default: system separator)
+ */
+const getRelativePath = (file, fromDir, separator = path.sep) => {
+  // format paths to the OS specific format
+  // (accounting for using the wrong seps)
+  let relativePath = path.relative(
+    formatPath(fromDir),
+    formatPath(file),
+  );
+
+  // Prefix the path if it is not already prefixed
+  if (!hasPathPrefix(relativePath)) {
+    relativePath = `.${separator}${relativePath}`;
+  }
+
+  return formatPath(relativePath, separator);
+};
+
+/**
+ * Ensures the direct for the specified filePath exists
+ *
+ * @param {String} filePath - Path to a file
+ */
+const ensureFileDirectoryExists = (filePath) => {
+  const directory = path.dirname(filePath);
+
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+};
+
+module.exports = {
+  getRelativePath,
+  ensureFileDirectoryExists,
+  formatPath,
+};
---
+++ src/utils/pathHelper.test.js
@@ -0,0 +1,179 @@
+const path = require('path');
+const faker = require('faker');
+const mockfs = require('mock-fs');
+const fs = require('fs');
+
+const { generateArray } = require('../utils/testUtils');
+const { getRelativePath, formatPath, ensureFileDirectoryExists } = require('./pathHelper');
+
+afterEach(() => mockfs.restore());
+
+describe('getRelativePath()', () => {
+  describe('prefixes', () => {
+    it('should resolve windows paths with right seps', () => {
+      const file = 'c:\\users\\george\\dev\\app\\src\\stuff.js';
+      const fromDir = 'c:\\users\\george\\dev\\app\\stories\\';
+
+      const actual = getRelativePath(file, fromDir, '\\');
+
+      expect(actual).toEqual('..\\src\\stuff.js');
+    });
+
+    it('should resolve windows paths with wrong seps', () => {
+      const file = 'c:/users/george/dev/app/src/stuff.js';
+      const fromDir = 'c:/users/george/dev/app/stories/';
+
+      const actual = getRelativePath(file, fromDir, '\\');
+
+      expect(actual).toEqual('..\\src\\stuff.js');
+    });
+
+    it('should resolve unix paths with right seps', () => {
+      const file = '/users/george/dev/app/src/stuff.js';
+      const fromDir = '/users/george/dev/app/stories/';
+
+      const actual = getRelativePath(file, fromDir, '/');
+
+      expect(actual).toEqual('../src/stuff.js');
+    });
+
+    it('should resolve unix paths with wrong seps', () => {
+      const file = '\\users\\george\\dev\\app\\src\\stuff.js';
+      const fromDir = '\\users\\george\\dev\\app\\stories\\';
+
+      const actual = getRelativePath(file, fromDir, '/');
+
+      expect(actual).toEqual('../src/stuff.js');
+    });
+
+    it('should be added when file is in the same folder', () => {
+      const relativePath = 'abc123';
+      const separator = '/';
+      const spy = jest.spyOn(path, 'relative');
+
+      spy.mockImplementation(() => relativePath);
+
+      const actual = getRelativePath(faker.system.fileName(), faker.system.fileName(), separator);
+
+      expect(actual).toEqual(`.${separator}${relativePath}`);
+
+      spy.mockReset();
+      spy.mockRestore();
+    });
+
+    it('should not change a path prefixed with "./"', () => {
+      const separator = '/';
+      const relativePath = `.${separator}abc123`;
+      const spy = jest.spyOn(path, 'relative');
+
+      spy.mockImplementation(() => relativePath);
+
+      const actual = getRelativePath(faker.system.fileName(), faker.system.fileName(), separator);
+
+      expect(actual).toEqual(relativePath);
+
+      spy.mockReset();
+      spy.mockRestore();
+    });
+
+    it('should not change a path prefixed with ".."', () => {
+      const separator = '/';
+      const relativePath = `..${separator}abc123`;
+      const spy = jest.spyOn(path, 'relative');
+
+      spy.mockImplementation(() => relativePath);
+
+      const actual = getRelativePath(faker.system.fileName(), faker.system.fileName(), separator);
+
+      expect(actual).toEqual(relativePath);
+
+      spy.mockReset();
+      spy.mockRestore();
+    });
+
+    it('should not change a path prefixed with ".\\"', () => {
+      const separator = '\\';
+      const relativePath = `.${separator}abc123`;
+      const spy = jest.spyOn(path, 'relative');
+
+      spy.mockImplementation(() => relativePath);
+
+      const actual = getRelativePath(faker.system.fileName(), faker.system.fileName(), separator);
+
+      expect(actual).toEqual(relativePath);
+
+      spy.mockReset();
+      spy.mockRestore();
+    });
+  });
+});
+
+describe('formatPath()', () => {
+  it('should convert unix style paths to windows style', () => {
+    const pathSegments = generateArray(() => faker.system.fileName(), 10);
+    const sourceDir = pathSegments.join('/');
+    const expected = pathSegments.join('\\');
+
+    const actual = formatPath(sourceDir, '\\');
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should convert windows style paths to unix style', () => {
+    const pathSegments = generateArray(() => faker.system.fileName(), 10);
+    const sourceDir = pathSegments.join('\\');
+    const expected = pathSegments.join('/');
+
+    const actual = formatPath(sourceDir, '/');
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should preserve windows style paths', () => {
+    const pathSegments = generateArray(() => faker.system.fileName(), 10);
+    const expected = pathSegments.join('\\');
+
+    const actual = formatPath(expected, '\\');
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should preservce unix style paths', () => {
+    const pathSegments = generateArray(() => faker.system.fileName(), 10);
+    const expected = pathSegments.join('/');
+
+    const actual = formatPath(expected, '/');
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('ensureFileDirectoryExists()', () => {
+  it('should create directory when it does not exist', () => {
+    const sourceFileDir = faker.random.word();
+    const sourceFile = [sourceFileDir, faker.system.fileName()].join(path.sep);
+    // mock fs defaults to your current working directory, so append paths to there
+    const expectedPath = path.join(process.cwd(), sourceFileDir);
+
+    mockfs({});
+
+    // confirm the dir doesn't already exist
+    expect(fs.existsSync(expectedPath)).toBe(false);
+
+    ensureFileDirectoryExists(sourceFile);
+
+    expect(fs.existsSync(expectedPath)).toBe(true);
+  });
+  it('should not create directory when already exist', () => {
+    const sourceFileDir = faker.random.word();
+    const sourceFile = [sourceFileDir, faker.system.fileName()].join(path.sep);
+    // mock fs defaults to your current working directory, so append paths to there
+    const expectedPath = path.join(process.cwd(), sourceFileDir);
+
+    mockfs({ [sourceFileDir]: { /* fake directory */ } });
+
+    ensureFileDirectoryExists(sourceFile);
+
+    expect(fs.existsSync(expectedPath)).toBe(true);
+  });
+});
